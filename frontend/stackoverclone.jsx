8f24c04c4cfc91713970fde001121a677c1b9122
kevinledev <kevinle.developer@gmail.com>
2022-03-16 21:30:11 -0700

    created functional app and root jsx

--- frontend/stackoverclone.jsx
+++ frontend/stackoverclone.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
-import {signup, login, logout} from './util/session_api_util'
+import Root from "./components/Root"
+
+// import {signup, login, logout} from './util/session_api_util'
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   const store = configureStore();
   //testing
-  window.login = login;
-  window.logout = logout;
-  window.signup = signup;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  // window.login = login;
+  // window.logout = logout;
+  // window.signup = signup;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
 
-  ReactDOM.render(<h1>entry file here</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });

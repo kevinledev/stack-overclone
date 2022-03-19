672c54ca103204e65a83fc603d5cbe123519a644
kevinledev <kevinle.developer@gmail.com>
2022-03-19 15:49:51 -0700

    auth routes for login and signup

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import GreetingContainer from "./NavBar/Greeting/GreetingContainer";
 import { Route } from 'react-router-dom'
+import { AuthRoute } from "../util/route_util";
 import LoginFormContainer from "./Session/LoginFormContainer";
 import SignupFormContainer from "./Session/SignupFormContainer";
 import NavBar from "./NavBar/NavBar"
 
+
 const App = () => (
   <div className="app">
     <NavBar />
-    <Route path="/login" component={LoginFormContainer} />
-    <Route path="/signup" component={SignupFormContainer} />
+    <AuthRoute path="/login" component={LoginFormContainer} />
+    <AuthRoute path="/signup" component={SignupFormContainer} />
     {/* <Route path="/users/:userId" component={UserShowContainer} /> */}
   </div>
 );

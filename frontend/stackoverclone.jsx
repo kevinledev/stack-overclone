297dc1e2ca54a241273794e0f565333b00f46027
kevinledev <kevinle.developer@gmail.com>
2022-03-19 15:40:34 -0700

    bootstrap current user

--- frontend/stackoverclone.jsx
+++ frontend/stackoverclone.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
-import Root from "./components/Root"
+import Root from "./components/Root";
 
-import {signup, login, logout} from './actions/session_actions'
+import { signup, login, logout } from "./actions/session_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  const store = configureStore();
-  
+
+  // const store = configureStore();
+
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser },
+      },
+      session: { currentUserId: window.currentUser.id },
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+
   //testing
   window.login = login;
   window.logout = logout;

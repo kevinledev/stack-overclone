a297c30e20bc6fc22c7d19b69cf0bd443f893818
kevinledev <kevinle.developer@gmail.com>
2022-03-26 23:10:17 -0700

    complete footer

--- frontend/components/Footer/Footer.jsx
+++ frontend/components/Footer/Footer.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="footer-container">footer here</div>
-  )
-}
+    <div className="footer-container">
+      <div className="footercenter">
+        <div className="footer-left">
+          <Link to={`/`}>
+            <img width="35px" src={window.loginLogo}></img>
+          </Link>
+          <div className="main-footer-links">
+            <div className="stackoverclone-link">
+              <Link to={`/`}>STACK OVERCLONE</Link>
+            </div>
+            <ul>
+              <Link to={`/questions`}>Questions</Link>
+              <Link to={`/users`} className="footer-userslink">Users</Link>
+            </ul>
+          </div>
+        </div>
+        <div className="dev-links-container">
+          <div className="dev-links">
+            <h1>DEVELOPER</h1>
+            <ul>
+              <a>LinkedIn</a>
+              <a className="footer-ghlink">Github</a>
+            </ul>
+          </div>
+          <div className="copyright">
+            &copy; 2022, Kevin Le. All rights reserved.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
